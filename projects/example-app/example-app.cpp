9befebbe00ce2983447e8de88f3b61a8845a7c82
ragh <rsprabhu@gmail.com>
2020-09-02 15:01:52 -0700

    update C++ code and notebook

--- projects/example-app/example-app.cpp
+++ projects/example-app/example-app.cpp
@@ -1,7 +1,34 @@
-#include <torch/torch.h>
-#include <iostream>
+#include <torch/script.h> // One-stop header.
 
-int main() {
-  torch::Tensor tensor = torch::eye(3);
-  std::cout << tensor << std::endl;
+#include <iostream>
+#include <memory>
+
+int main(int argc, const char* argv[]) {
+  if (argc != 2) {
+    std::cerr << "usage: example-app <path-to-exported-script-module>\n";
+    return -1;
+  }
+  
+  torch::jit::script::Module module;
+  try {
+    // Deserialize the ScriptModule from a file using torch::jit::load().
+    module = torch::jit::load(argv[1]);
+  }
+  catch (const c10::Error& e) {
+    std::cerr << "error loading the model\n";
+    return -1;
+  }
+
+  std::cout << "Model "<< argv[1]<<" loaded fine\n";
+
+  // Create a vector of inputs.
+  std::vector<torch::jit::IValue> inputs;
+  inputs.push_back(torch::randn({1, 1, 64, 101}));
+
+  // Execute the model and turn its output into a tensor.
+  at::Tensor output = module.forward(inputs).toTensor();
+  std::cout << output << "\n";
+  int y_hat = output.argmax(1).item().toInt();
+  std::cout << "Predicted class: " << y_hat <<"\n";
+
 }
